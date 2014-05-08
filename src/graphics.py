7e43008f202e092bf72d31e57cb3c03575a661cb
JulienRouse <julien.rouse@gmail.com>
2014-05-09 00:17:13 +0200

    ajout dun menu

--- src/graphics.py
+++ src/graphics.py
@@ -1,6 +1,19 @@
-from PyQt5.QtWidgets import (QApplication,QWidget,QGridLayout)
+from PyQt5.QtWidgets import (QDialog,QApplication,QWidget,QGridLayout,QLineEdit,QVBoxLayout,QMainWindow,QAction)
+from PyQt5.QtGui import QIcon, QKeySequence
 
-class  Fenetre(QWidget):
+class FenetreCentrale(QDialog):
+    def __init__(self, parent=None):
+        super(FenetreCentrale,
+              self).__init__(parent)
+        lineEdit = QLineEdit()
+        lineEdit2 = QLineEdit()
+
+        layout = QVBoxLayout()
+        layout.addWidget(lineEdit)
+        layout.addWidget(lineEdit2)
+        self.setLayout(layout)
+
+class  Fenetre(QMainWindow):
     def __init__(self, parent=None):
         super(Fenetre,self).__init__(parent)
         self.initUi()
@@ -8,8 +21,68 @@ class  Fenetre(QWidget):
     def initUi(self):
        
         self.setWindowTitle("ClassManager")
+        
+        
+        self.dialog = FenetreCentrale()
+        self.setCentralWidget(self.dialog)
+        
 
+        self.createActions()
+        self.createMenus()
+        self.resize(360,145)
+        self.show()
     
+
+        
+        
+    
+    def blabla(self):
+        pass
+
+    def createActions(self):
+        self.newAct = QAction("&New", self,
+            shortcut=QKeySequence.New, statusTip="Create a new file",
+                              triggered=self.blabla)
+        
+        self.openAct = QAction( "&Open...", self,
+                shortcut=QKeySequence.Open, statusTip="Open an existing file",
+                               triggered=self.blabla)
+
+        self.saveAct = QAction( "&Save", self,
+                shortcut=QKeySequence.Save,
+                               statusTip="Save the document to disk", triggered=self.blabla)
+
+        self.saveAsAct = QAction("Save &As...", self,
+                shortcut=QKeySequence.SaveAs,
+                statusTip="Save the document under a new name",
+                                 triggered=self.blabla)
+
+        self.exitAct = QAction("&Exit", self, shortcut="Ctrl+Q",
+                statusTip="Exit the application", triggered=self.close)
+
+    def createToolBars(self):
+        self.fileToolBar = self.addToolBar("File")
+        self.fileToolBar.addAction(self.newAct)
+        self.fileToolBar.addAction(self.openAct)
+        self.fileToolBar.addAction(self.saveAct)
+
+    def createMenus(self):
+        """ self.fileMenu = self.menuBar()
+        self.fileMenu.addMenu("&File")
+        self.fileMenu.addAction(self.newAct)
+        self.fileMenu.addAction(self.openAct)
+        self.fileMenu.addAction(self.saveAct)
+        """
+
+        self.menuBar = self.menuBar()
+        self.fileMenu = self.menuBar.addMenu("&File")
+        self.fileMenu.addAction(self.newAct)
+        self.fileMenu.addAction(self.openAct)
+
+
+
+
+
 
 if __name__ == '__main__':
     import sys
@@ -20,6 +93,6 @@ if __name__ == '__main__':
    
 
    
-    fenetre.show()
+    
     
     sys.exit(app.exec_())
