19fac50d7a5afa3035ec3f2d06c31edc4a3e4c49
JulienRouse <julien.rouse@gmail.com>
2014-05-09 03:12:05 +0200

    ClassManager

--- src/metier.py
+++ src/metier.py
@@ -1,1 +1,17 @@
-print "v"
+"""
+Metier: Module qui gere la partie calcul de ClassManager
+
+"""
+
+class ClassManager(object):
+
+    def __init__(self):
+        self.listClass = []
+        
+    def createClass(self):
+        new_class = Class()
+        return new_class
+
+    def deleteClass(self,classToRemove):
+        pass
+    
