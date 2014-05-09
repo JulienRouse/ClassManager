5fcf648c1ddd3f439ee86868476c17a81bc1f312
JulienRouse <julien.rouse@gmail.com>
2014-05-09 16:09:28 +0200

    ajout de la classe CarnetDeNote

--- src/metier.py
+++ src/metier.py
@@ -2,6 +2,18 @@
 Metier: Module qui gere la partie calcul de ClassManager
 
 """
+class Matiere(object):
+    def __init__(self,name):
+        self.name = name
+        self.commentaire = ""
+
+class CarnetDeNote(object):
+    def __init__(self):
+        self.moyenneGenerale = -1
+        self.appreciationGenerale = "No Comment"
+        self.ListMatiere = []
+        
+
 class Eleve(object):
     def __init__(self,name,surname):
         self.name = name
@@ -50,7 +62,10 @@ class ClassManager(object):
         classeId = len(self.listClass)
         new_Classe.setId(classeId)
         return new_Classe
-
+    
+    def addClasse(self, classe):
+        
+    
     def deleteClasse(self,classeToRemove):
         pass
     
