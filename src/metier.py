1d3d5c11ea5c11bf287c329aeffdbaf7427d8093
JulienRouse <julien.rouse@gmail.com>
2014-05-09 15:33:44 +0200

    ajout des classes Class, Eleve

--- src/metier.py
+++ src/metier.py
@@ -2,16 +2,55 @@
 Metier: Module qui gere la partie calcul de ClassManager
 
 """
+class Eleve(object):
+    def __init__(self,name,surname):
+        self.name = name
+        self.surname = surname
+        self.eleveId = -1
+        self.classeId = -1
+    
+    def setId(self,eleveId,classeId):
+        self.eleveId = eleveId
+        self.classeId = classeId
+    
+    def printEleve(self):
+        print "name:", self.name
+        print "surname:", self.surname
+        print "id:", self.eleveId
+        print "class:", self.classeId
+
+class Classe(object):
+    def __init__(self,name):
+        self.name = name
+        self.listEleves = []
+        self.classeId = -1
+    
+    def setId(self, classeId):
+        self.classeId = classeId
+
+    def addEleve(self,eleve):
+        eleveId = len(self.listEleves)
+        eleve.setId(eleveId,self.classeId)
+        self.listEleves.append(eleve)
+        
+    def removeEleve(self,eleve):
+        pass
+
+    def printClasse(self):
+        for elt in self.listEleves:
+            elt.printEleve()
 
 class ClassManager(object):
 
     def __init__(self):
         self.listClass = []
         
-    def createClass(self):
-        new_class = Class()
-        return new_class
+    def createClasse(self, name='ClassDefault'):
+        new_Classe = Classe(name)
+        classeId = len(self.listClass)
+        new_Classe.setId(classeId)
+        return new_Classe
 
-    def deleteClass(self,classToRemove):
+    def deleteClasse(self,classeToRemove):
         pass
     
