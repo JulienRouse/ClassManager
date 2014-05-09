5d2a5ca3f346de2c68a6c7e823eac11e4f25ae5e
JulienRouse <julien.rouse@gmail.com>
2014-05-09 15:31:20 +0200

    ajout du fichier startup.py pour avoir une base dans le shell interactif

---
+++ src/startup.py
@@ -0,0 +1,16 @@
+from metier import ClassManager, Classe, Eleve
+
+
+cm = ClassManager()
+c1 = cm.createClasse("cp1")
+cm.listClass.append(c1)
+
+e1 = Eleve('rouse','ju')
+e1.printEleve()
+e2 = Eleve('toto','ti')
+e2.printEleve()
+
+cm.listClass[0].addEleve(e1)
+cm.listClass[0].addEleve(e2)
+cm.listClass[0].printClasse()
+
