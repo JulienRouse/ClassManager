5ed4913c33bac865ac8a59a415719ee073c49174
JulienRouse <julien.rouse@gmail.com>
2014-05-12 17:09:48 +0200

    ajout de initDictMatiere et de printMatiereName pour CarnetDeNote et de calcMoyenne et addTest pour Matiere

--- src/metier.py
+++ src/metier.py
@@ -3,16 +3,57 @@ Metier: Module qui gere la partie calcul de ClassManager
 
 """
 class Matiere(object):
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.commentaire = ""
+        self.listTest = []
+        self.moyenne = -1
+        
+    def addTest(self, date, note, commentaire='No Comment'):
+        """
+        date au format 'jj/mm/yy'
+        0<=note<=20
+        """
+        self.listTest.append((date,note,commentaire))
+        self.calcMoyenne()
+
+    def calcMoyenne(self):
+        """
+        calcule la moyenne des notes contenues
+        dans listTest
+        si listTest est vide, la moyenne vaut -1
+        """
+        if len(self.listTest) == 0:
+            self.moyenne = -1
+        tmp = [m[1] for m in self.listTest]
+        moy = sum(tmp) / float(len(tmp))
+        self.moyenne = moy
 
 class CarnetDeNote(object):
     def __init__(self):
         self.moyenneGenerale = -1
         self.appreciationGenerale = "No Comment"
-        self.ListMatiere = []
+        self.initDictMatiere("../misc/matiere_config.txt")
+    
+    def initDictMatiere(self,filename):
+        """
+        lit un fichier filename de format key name_matiere
+        pour creer toutes les matieres presentes dans le carnet de note
+        """
+        self.dictMatiere = {}
+        with open(filename, 'r') as f:
+            for line in f:
+                l = line.split()
+                #on cree un dictionnaire contenant
+                #les matieres contenus dans filename
+                self.dictMatiere[l[0]]=Matiere(l[1])
         
+
+    def printMatiereName(self):
+        for key in self.dictMatiere:
+            print self.dictMatiere[key].name,
+        print ""
+
 
 class Eleve(object):
     def __init__(self,name,surname):
@@ -20,6 +61,7 @@ class Eleve(object):
         self.surname = surname
         self.eleveId = -1
         self.classeId = -1
+        self.cdn = CarnetDeNote()
     
     def setId(self,eleveId,classeId):
         self.eleveId = eleveId
@@ -30,6 +72,7 @@ class Eleve(object):
         print "surname:", self.surname
         print "id:", self.eleveId
         print "class:", self.classeId
+        self.cdn.printMatiereName()
 
 class Classe(object):
     def __init__(self,name):
@@ -64,8 +107,9 @@ class ClassManager(object):
         return new_Classe
     
     def addClasse(self, classe):
-        
+        pass
     
     def deleteClasse(self,classeToRemove):
         pass
     
+
